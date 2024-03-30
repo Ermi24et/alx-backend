299473211685b6a59ee4da39a02bd981dc415c5f
Ermi24et <lilermi24@gmail.com>
2024-03-30 23:01:36 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,6 +27,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        paginate dataset
+        """
         assert isinstance(page, int)
         assert isinstance(page_size, int)
         assert page > 0
