336d3098327aa2687ab7bf22d94ba0628b3d4f19
Ermi24et <lilermi24@gmail.com>
2024-04-02 19:14:48 +0300

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+FIFO caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """a method that is responsible to do FIFO caching"""
+    def __init__(self):
+        super().__init__()
+    def put(self, key, item):
+        """a method to assign the dict self.cache_data the item value for the key"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                discarded = self.cache_data.pop(first_key)
+                print(f"DISCARD: {discarded[0]}")
+    def get(self, key):
+        """a method that returns the value in self.cache_data linked to key"""
+        if key is None and key not in self.cache_data:
+            return None
+        return self.cache_data[key]
