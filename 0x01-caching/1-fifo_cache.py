52a424bf3a05bf728a1450523849927992ad1269
Ermi24et <lilermi24@gmail.com>
2024-04-02 22:59:12 +0300

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,11 +15,11 @@ class FIFOCache(BaseCaching):
         """a method to assign the dict self.cache_data the
         item value for the key"""
         if key is not None and item is not None:
-            self.cache_data[key] = item
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = next(iter(self.cache_data))
                 self.cache_data.pop(first_key)
                 print(f"DISCARD: {first_key}")
+                self.cache_data[key] = item
             else:
                 self.cache_data[key] = item
 
