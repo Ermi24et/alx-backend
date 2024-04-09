059469c0d1921ceb4cc4057f4d7fcac29b0c7279
Ermi24et <lilermi24@gmail.com>
2024-04-09 19:57:56 +0300

    get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+from flask.babel import Babel
+from flask import Flask, request, render_template
+
+
+app = Flask()
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def welcome():
+    """simple method to render an html file"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
